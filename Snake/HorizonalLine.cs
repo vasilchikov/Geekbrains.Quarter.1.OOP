cb6fcc60833a56ed32af4e58e3cd75a0bcb647d1
Vasilchikov <vasdm2018@yandex.ru>
2021-06-23 00:58:24 +0300

    Класс «Горизонтальные линии»

---
+++ Snake/HorizonalLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class HorizonalLine
+    {
+        private List<Point> pList;
+        public HorizonalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
