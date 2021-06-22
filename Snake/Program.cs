a0711e8f74f25a3f2a5514adda2fa2899e76f8aa
Vasilchikov <vasdm2018@yandex.ru>
2021-06-23 00:58:24 +0300

    Класс «Горизонтальные линии»

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,54 +8,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            List<int> numList = new List<int>();
-            numList.Add(4);
-            numList.Add(3);
-            numList.Add(7);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            numList.RemoveAt(2);
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(3,11,'#');
-            List<Point> points = new List<Point> {p1, p2};
-            foreach (Point point in points)
-            {
-                Console.WriteLine(point.x);
-                Console.WriteLine(point.y);
-                Console.WriteLine(point.sym);
-            }
+            HorizonalLine horizonalLine = new HorizonalLine(0, 45, 10, '+');
+            horizonalLine.Draw();
+            VerticalLine verticalLine = new VerticalLine(0, 10, 45, '#');
+            verticalLine.Draw();
         }
-
-        public static void Func1(int value)
-        {
-
-        }
-
-        public static void Func2(int value)
-        {
-            value = value + 1;
-        }
-
-        public static void Func3(int x)
-        {
-            x = x + 1;
-        }
-
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
-        }
-
-        public static void Update(Point p)
-        {
-            p = new Point();
-        }
-	}
+    }
 }
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        private List<Point> pList = new List<Point>();
+
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point point in pList)
+            {
+                point.Draw();
+            }
+        }
+    }
+}
