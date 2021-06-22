cc10bfa2d0fda94d69861cae0fc35e1de02ceda5
Vasilchikov <vasdm2018@yandex.ru>
2021-06-22 14:48:19 +0300

    Создали класс Point

---
+++ Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(sym);
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,21 +6,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 4;
-            int y1 = 8; 
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-
-            int x2 = 2;
-            int y2 = 3;
-            char sym2 = '#';
-            Draw(x2,y2,sym2);
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x,y);
-            Console.Write(sym);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+            p1.Draw();
         }
     }
 }
