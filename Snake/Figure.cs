00348721e8be1ad92e63ce61de77a1aa044ad21f
Vasilchikov <vasdm2018@yandex.ru>
2021-07-04 15:26:19 +0300

    Класс «Вертикальные линии» и «Фигура» (Наследование)

---
+++ Snake/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Figure
+    {
+        protected List<Point> pList;
+        public void Draw()
+        {
+            foreach (Point points in pList)
+            {
+                points.Draw();
+            }
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,10 +8,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            HorizonalLine horizonalLine = new HorizonalLine(0, 45, 10, '+');
-            horizonalLine.Draw();
-            VerticalLine verticalLine = new VerticalLine(0, 10, 45, '#');
-            verticalLine.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+
+            HorizonalLine upLine = new HorizonalLine(0, 78, 0, '+');
+            HorizonalLine downLine = new HorizonalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+            Console.ReadKey();
         }
     }
 }
